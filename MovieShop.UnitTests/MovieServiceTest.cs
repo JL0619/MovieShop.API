a6e13a510d2ab8160b2d0d7d492aa545ff40bbb3
Simin <liangsimin0619@gmail.com>
2019-11-13 15:40:19 -0500

    Added Unit Test Projects

---
+++ MovieShop.UnitTests/MovieServiceTest.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieShop.UnitTests
+{
+    [TestClass]
+    public class MovieServiceTest
+    {
+        [TestMethod]
+        public void TestMovieFromFakeData1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMovieFromFakeData2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMovieFromFakeData3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMovieFromFakeData4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMovieFromFakeData5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+    }
+}
